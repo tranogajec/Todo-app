c0bf80f37614412ead00030e28822639a58c9b78
10chi <tenaranogajec.1@gmail.com>
2022-05-20 23:28:07 +0200

    hardcode text inputs and add buttons

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import React from 'react';
-
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  View,
+} from 'react-native';
 
 const App = () => {
+  const [todoInput, setTodoInput] = useState('');
+  const [listInput, setListInput] = useState('');
+
+  function handleTodoInputChange(value) {
+    setTodoInput(value);
+  }
+
+  function addTodo() {
+    console.log(todoInput);
+  }
+
+  function handleListInputChange(value) {
+    setListInput(value);
+  }
+
+  function addListToDropdown() {
+    console.log(listInput);
+  }
+
   return (
     <SafeAreaView>
       <View>
-        <Text>Todo app</Text>
+        <Text style={styles.appTitle}>Todo app</Text>
+        <View style={styles.inputSection}>
+          <TextInput
+            onChangeText={handleTodoInputChange}
+            value={todoInput}
+            placeholder="Add your todo"
+          />
+          <Button onPress={() => addTodo()} title="+" />
+        </View>
+        <View>
+          <TextInput
+            onChangeText={handleListInputChange}
+            value={listInput}
+            placeholder="Add new list"
+          />
+          <Button onPress={() => addListToDropdown()} title="+" />
+        </View>
       </View>
     </SafeAreaView>
   );
