a4a148f66e18ed37e9ddae7664b0e582fc6d9646
10chi <tenaranogajec.1@gmail.com>
2022-05-20 21:00:43 +0200

    add-initial-project-structure

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Todo app</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
